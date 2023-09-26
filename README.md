23dc41cfa9c63c4148480ad98cb0e93d4fe90437
stasbaz <bazuluk989@gmail.com>
2023-09-26 02:38:14 -0600

    Final version of app

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# weather-programm
+# Weather Programm
+
+This is a web application that allows users to check the current weather and 5-day forecast for a specific city. Users can also save their recent searches and revisit them later.
+
+## Quick-view
+There is a link to deployed app - https://stasbaz.github.io/weather-programm/
+![And screenshot](./assets/127.0.0.1_5500_%20(1).png)
+
+## Features
+Users can enter a city name and view the current weather data, including temperature, wind speed, humidity, and an icon representing the weather conditions.
+
+In addition to current weather data, users can see a 5-day weather forecast that includes date, weather icon, temperature, wind speed, and humidity.
+
+The application stores a list of recent searches, and users can click on a previous search to quickly retrieve the weather data for that city.
+
+## License
+This project is licensed under the MIT License - see the LICENSE file for details.
--- assets/script.js
+++ assets/script.js
@@ -1,107 +1,3 @@
-function getWeatherData() {
-    var cityName = document.getElementById('city-input').value;
-    var apiKey = '6ad917cff07da33c01d8bf9fd231fa55';
-
-    if (!cityName) {
-        alert('Please enter a city name.');
-        return;
-    }
-
-    var currentWeatherData;
-    var forecastData;
-
-    var currentApiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + apiKey;
-    var forecastApiUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&appid=' + apiKey;
-
-    Promise.all([fetch(currentApiUrl), fetch(forecastApiUrl)])
-        .then((responses) => Promise.all(responses.map((response) => response.json())))
-        .then(([currentResponse, forecastResponse]) => {
-            if (!currentResponse || !forecastResponse) {
-                throw new Error('API data missing');
-            }
-
-            if (currentResponse.cod !== 200 || forecastResponse.cod !== '200') {
-                throw new Error('City not found or data is incomplete');
-            }
-
-            currentWeatherData = currentResponse;
-            forecastData = forecastResponse;
-
-            displayWeatherData(currentWeatherData);
-            displayForecastData(forecastData);
-
-            var searchData = {
-                current: currentWeatherData,
-                forecast: forecastData,
-            };
-            localStorage.setItem(cityName, JSON.stringify(searchData));
-        })
-        .catch((error) => {
-            alert(error.message);
-            console.error('Error with operation:', error);
-        });
-}
-
-function displayForecastData(forecastData) {
-    var forecastContainer = document.getElementById('forecast-container');
-
-    if (forecastContainer) {
-        forecastContainer.innerHTML = '';
-
-        var titleElement = document.createElement('h2');
-        titleElement.textContent = '5-Day Forecast:';
-        forecastContainer.appendChild(titleElement);
-        titleElement.classList.add('forecast-title');
-
-        for (let i = 0; i < forecastData.list.length; i += 8) {
-            var forecastDay = forecastData.list[i];
-            var date = new Date(forecastDay.dt * 1000);
-            var forecastDiv = document.createElement('div');
-            forecastDiv.classList.add('forecast-item');
-            
-            var dateElement = document.createElement('p');
-            dateElement.textContent = date.toLocaleDateString();
-
-            var iconCode = forecastDay.weather[0].icon;
-            var iconUrl = `http://openweathermap.org/img/w/${iconCode}.png`;
-            var iconElement = document.createElement('img');
-            iconElement.setAttribute('src', iconUrl);
-            iconElement.alt = 'Weather Icon';
-
-            var temperatureInCelsius = forecastDay.main.temp - 273.15;
-            var temperatureInFahrenheit = (temperatureInCelsius * 9 / 5) + 32;
-
-            var temperatureElement = document.createElement('p');
-            temperatureElement.textContent = `Temperature: ${temperatureInFahrenheit.toFixed(2)} °F`;
-
-            var windSpeed = forecastDay.wind.speed;
-            var windElement = document.createElement('p');
-            windElement.textContent = `Wind Speed: ${windSpeed} m/s`;
-
-            var humidity = forecastDay.main.humidity;
-            var humidityElement = document.createElement('p');
-            humidityElement.textContent = `Humidity: ${humidity}%`;
-
-            forecastDiv.appendChild(dateElement);
-            forecastDiv.appendChild(iconElement);
-            forecastDiv.appendChild(temperatureElement);
-            forecastDiv.appendChild(windElement);
-            forecastDiv.appendChild(humidityElement);
-            forecastContainer.appendChild(forecastDiv);
-        }
-    } else {
-        console.error('Forecast container not found in the DOM.');
-    }
-}
-
-document.getElementById('search-button').addEventListener('click', getWeatherData);
-
-function saveWeatherData(city, data) {
-    var savedWeatherData = JSON.parse(localStorage.getItem('weatherData')) || {};
-    savedWeatherData[city] = data;
-    localStorage.setItem('weatherData', JSON.stringify(savedWeatherData));
-}
-
 function displayWeatherData(data) {
     var weatherDataElement = document.getElementById('weather-data');
     weatherDataElement.innerHTML = '';
@@ -138,8 +34,130 @@ function displayWeatherData(data) {
         weatherDataElement.appendChild(temperatureElementFahrenheit);
         weatherDataElement.appendChild(windElement);
         weatherDataElement.appendChild(humidityElement);
+
+        createRecentSearchButton(cityName);
     } else {
         console.error('Unexpected weather data structure:', data);
         weatherDataElement.textContent = 'Weather data not available';
     }
 }
+
+function displayForecastData(forecastData) {
+    const forecastContainer = document.getElementById('forecast-container');
+
+    if (forecastContainer) {
+        forecastContainer.innerHTML = '';
+
+        var titleElement = document.createElement('h2');
+        titleElement.textContent = '5-Day Forecast:';
+        forecastContainer.appendChild(titleElement);
+        titleElement.classList.add('forecast-title');
+
+        for (let i = 0; i < forecastData.list.length; i += 8) {
+            var forecastDay = forecastData.list[i];
+            var date = new Date(forecastDay.dt * 1000);
+            var forecastDiv = document.createElement('div');
+            forecastDiv.classList.add('forecast-item');
+
+            var dateElement = document.createElement('p');
+            dateElement.textContent = date.toLocaleDateString();
+
+            var iconCode = forecastDay.weather[0].icon;
+            var iconUrl = `http://openweathermap.org/img/w/${iconCode}.png`;
+            var iconElement = document.createElement('img');
+            iconElement.setAttribute('src', iconUrl);
+            iconElement.alt = 'Weather Icon';
+
+            var temperatureInCelsius = forecastDay.main.temp - 273.15;
+            var temperatureInFahrenheit = (temperatureInCelsius * 9 / 5) + 32;
+
+            var temperatureElement = document.createElement('p');
+            temperatureElement.textContent = `Temperature: ${temperatureInFahrenheit.toFixed(2)} °F`;
+
+            var windSpeed = forecastDay.wind.speed;
+            var windElement = document.createElement('p');
+            windElement.textContent = `Wind Speed: ${windSpeed} m/s`;
+
+            var humidity = forecastDay.main.humidity;
+            var humidityElement = document.createElement('p');
+            humidityElement.textContent = `Humidity: ${humidity}%`;
+            forecastDiv.appendChild(dateElement);
+            forecastDiv.appendChild(iconElement);
+            forecastDiv.appendChild(temperatureElement);
+            forecastDiv.appendChild(windElement);
+            forecastDiv.appendChild(humidityElement);
+            forecastContainer.appendChild(forecastDiv);
+        }
+    } else {
+        console.error('Forecast container not found in the DOM.');
+    }
+}
+
+function createRecentSearchButton(cityName) {
+    var recentSearches = document.getElementById('recent-searches');
+    var button = document.createElement('button');
+    button.textContent = cityName;
+
+    button.addEventListener('click', function () {
+        getWeatherDataFromHistory(cityName);
+    });
+    recentSearches.appendChild(button);
+}
+
+function getWeatherDataFromHistory(cityName) {
+    var storedWeatherData = JSON.parse(localStorage.getItem(cityName));
+    if (storedWeatherData) {
+        displayWeatherData(storedWeatherData);
+
+        const forecastApiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=8a7a3bfd140630c61554fb2f7ec043d9`;
+        fetch(forecastApiUrl)
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error('Network Error');
+                }
+                return response.json();
+            })
+            .then((forecastData) => {
+                displayForecastData(forecastData);
+            })
+            .catch((error) => {
+                console.error('Error with operation:', error);
+            });
+    }
+}
+
+function getWeatherData() {
+    var cityName = document.getElementById('city-input').value;
+    var apiKey = '8a7a3bfd140630c61554fb2f7ec043d9';
+    if (!cityName) {
+        alert('Please enter a city name.');
+        return;
+    }
+
+    const currentWeatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+    fetch(currentWeatherApiUrl)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error('Network Error');
+            }
+            return response.json();
+        })
+        .then((data) => {
+            displayWeatherData(data);
+
+            localStorage.setItem(cityName, JSON.stringify(data));
+
+            const forecastApiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`;
+            return fetch(forecastApiUrl);
+        })
+        .then((response) => response.json())
+        .then((forecastData) => {
+            displayForecastData(forecastData);
+        })
+        .catch((error) => {
+            console.error('Error with operation:', error);
+            alert('City not found or data is incomplete. Please enter a valid city name.');
+        });
+}
+
+document.getElementById('search-button').addEventListener('click', getWeatherData);
